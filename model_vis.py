76988ddc7e36546f64615d2357bf5c3462502c67
JarredAllen <jarredallen73@gmail.com>
2020-05-28 16:59:14 -0700

    Plotted a visualization of r

--- model_vis.py
+++ model_vis.py
@@ -16,8 +16,29 @@ def plot_g(thetas, show=True):
     if show:
         plt.show()
 
-def main():
+def g_vs_theta():
     plot_g([1, 5, 10, 30, 100, -100])
+
+def r_vs_majority_time():
+    xs = np.linspace(0, 0.5, 2000)[1:]
+    ys = np.ceil(np.log(0.5)/np.log(1-xs))
+    fig, ax = plt.subplots()
+    ax.set_yscale("log")
+    ax.set_ylim(bottom=1)
+    ax.set_yticks([1, 10, 100, 1000, 10000])
+    ax.set_yticklabels(['1', '10', '100', '1000', '10000'])
+    ax.set_ylabel("Number of witnesses to overturn a majority")
+    ax.set_xscale("log")
+    ax.set_xticks([0.001, 0.01, 0.1, 0.5])
+    ax.set_xticklabels(['0.001', '0.01', '0.1', '0.5'])
+    ax.set_xlabel("Value of r")
+    ax.grid(which='minor', linewidth=0.3)
+    ax.grid(which='major', linewidth=0.8)
+    ax.plot(xs, ys)
+    plt.show()
+
+def main():
+    r_vs_majority_time()
 
 if __name__ == '__main__':
     main()
