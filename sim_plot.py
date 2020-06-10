a1c3aa4a6d6ebe32b1fd15c643c0f7b50bbec55e
JarredAllen <jarredallen73@gmail.com>
2020-06-10 16:25:42 -0700

    Added another plot for spectrum of dependence

--- sim_plot.py
+++ sim_plot.py
@@ -136,7 +136,8 @@ def main():
     # plot_prob_affirm_vs_position_with_initial_g([0.5, 0.75, 0.9, 0.95, 1.0], theta=2.0, initial_g=0.7)
     # plot_prob_affirm_vs_position_with_initial_g([0.5, 0.75, 0.9, 0.95, 1.0], theta=2.0, initial_g=0.7, agent_count=2000)
     # visualize_agreement([2.0, 5.0, 7.0, 10, 20], agent_count=100, num_reps=5000)
-    plot_prob_h_given_e_for_lambdas(np.linspace(0, 1, 100), 0.95, 0.5, 1e-3, 150, 5.0, 0.035, 1600, 8000)
+    # plot_prob_h_given_e_for_lambdas(np.linspace(0, 1, 100), 0.95, 0.5, 1e-3, 150, 5.0, 0.035, 1600, 8000)
+    plot_prob_h_given_e_for_lambdas(np.linspace(0, 1, 100), 0.7, 0.5, 1e-3, 40, 5.0, 0.035, 1600, 10000)
 
 if __name__ == '__main__':
     main()
